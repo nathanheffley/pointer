0b74230b9da5a76ccf898fd6aaa742f018a84301
Nathan Heffley <nathan@nathanheffley.com>
2018-11-21 17:52:05 -0500

    Improve tests for login page

--- test/unit/specs/LoginPage.spec.js
+++ test/unit/specs/LoginPage.spec.js
@@ -1,62 +1,77 @@
-import LoginPage from '@/components/LoginPage'
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
+import LoginPage from '@/components/LoginPage'
 
 describe('LoginPage.vue', () => {
-  const wrapper = shallowMount(LoginPage, {
-    stubs: {
-      RouterLink: RouterLinkStub
-    }
-  })
-
-  it('should let the user enter their name', () => {
-    expect(wrapper.vm.name).toBeNull()
-    wrapper.find('input#name').setValue('John Doe')
-    expect(wrapper.vm.name).toBe('John Doe')
-  })
-
-  it('should let the user enter a session ID', () => {
-    expect(wrapper.vm.session).toBeNull()
-    wrapper.find('input#room').setValue('123')
-    expect(wrapper.vm.session).toBe('123')
-  })
-
   it('should render disabled button without name and session', () => {
-    wrapper.setData({
-      session: null,
-      name: null
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
     })
+
+    expect(wrapper.findAll('button').length).toBe(1)
     expect(wrapper.find('button:disabled').exists()).toBe(true)
   })
 
-  it('should render disabled button with just name', () => {
-    wrapper.setData({
-      session: null,
-      name: 'John Doe'
+  it('should render disabled button with just name entered', () => {
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
     })
+    wrapper.find('input#name').setValue('John Doe')
+
+    expect(wrapper.findAll('button').length).toBe(1)
     expect(wrapper.find('button:disabled').exists()).toBe(true)
   })
 
   it('should render disabled button with just session', () => {
-    wrapper.setData({
-      session: '123',
-      name: null
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
     })
+    wrapper.find('input#room').setValue('123')
+
+    expect(wrapper.findAll('button').length).toBe(1)
+    expect(wrapper.find('button:disabled').exists()).toBe(true)
+  })
+
+  it('should render disabled button with empty string name and session', () => {
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    wrapper.find('input#name').setValue('   ')
+    wrapper.find('input#room').setValue('   ')
+
+    expect(wrapper.findAll('button').length).toBe(1)
     expect(wrapper.find('button:disabled').exists()).toBe(true)
   })
 
   it('should render enabled button with name and session', () => {
-    wrapper.setData({
-      session: '123',
-      name: 'John Doe'
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
     })
+    wrapper.find('input#name').setValue('John Doe')
+    wrapper.find('input#room').setValue('123')
+
+    expect(wrapper.findAll('button').length).toBe(1)
     expect(wrapper.find('button:disabled').exists()).toBe(false)
   })
 
   it('should link the user to the session they typed in', () => {
-    wrapper.setData({
-      session: '123',
-      name: 'John Doe'
+    const wrapper = shallowMount(LoginPage, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
     })
+    wrapper.find('input#name').setValue('John Doe')
+    wrapper.find('input#room').setValue('123')
+
     expect(wrapper.find(RouterLinkStub).props().to)
     .toEqual({
       name: 'SessionPage',
