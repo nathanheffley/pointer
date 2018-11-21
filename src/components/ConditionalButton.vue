b22cdd3561f8376a0409fd11f63c21b8206ad253
Nathan Heffley <nathan@nathanheffley.com>
2018-11-21 17:42:26 -0500

    Add tests for conditional button

--- src/components/ConditionalButton.vue
+++ src/components/ConditionalButton.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <button v-if="enabled" @click="$emit('click')" class="mt-2 block w-full btn btn-blue"><slot></slot></button>
+    <button v-if="enabled" @click="click" class="mt-2 block w-full btn btn-blue"><slot></slot></button>
     <button v-else class="mt-2 block w-full btn btn-blue hover:bg-blue opacity-50 cursor-not-allowed" disabled><slot></slot></button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['enabled']
+  props: ['enabled'],
+  methods: {
+    click: function () {
+      this.$emit('click')
+    }
+  }
 }
 </script>
