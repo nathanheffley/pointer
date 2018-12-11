394ccafe40ca5ad58b2a0a4a8ff880b9011d6593
Nathan Heffley <nathan@nathanheffley.com>
2018-12-10 22:35:33 -0500

    Start adding auth endpoint as lambda

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   FRONTEND_HOST: '"http://localhost:8080"',
-  API_HOST: '"http://localhost:9090"'
+  API_HOST: '"http://localhost:9000/.netlify/functions"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 module.exports = {
   NODE_ENV: '"production"',
   FRONTEND_HOST: '"https://pointer.nathanheffley.com"',
-  API_HOST: '"https://api.pointer.nathanheffley.com"',
+  API_HOST: '"https://api.pointer.nathanheffley.com/.netlify/functions"',
+  PUSHER_APP_ID: `"${process.env.PUSHER_APP_ID}"`,
   PUSHER_KEY: `"${process.env.PUSHER_KEY}"`,
+  PUSHER_SECRET: `"${process.env.PUSHER_SECRET}"`,
   PUSHER_CLUSTER: `"${process.env.PUSHER_CLUSTER}"`
 }
