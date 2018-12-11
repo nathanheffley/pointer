adcf3bc8cf64385818eb46acf0df239819da67ab
Nathan Heffley <nathan@nathanheffley.com>
2018-12-10 21:18:54 -0500

    Use just dotenv

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,6 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-const Dotenv = require('dotenv-webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -75,8 +74,5 @@ module.exports = {
         }
       }
     ]
-  },
-  plugins: [
-    new Dotenv()
-  ]
+  }
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,10 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
   NODE_ENV: '"production"',
   FRONTEND_HOST: '"https://pointer.nathanheffley.com"',
-  API_HOST: '"https://api.pointer.nathanheffley.com"'
+  API_HOST: '"https://api.pointer.nathanheffley.com"',
+  PUSHER_KEY: `"${process.env.PUSHER_KEY}"`,
+  PUSHER_CLUSTER: `"${process.env.PUSHER_CLUSTER}"`
 }
