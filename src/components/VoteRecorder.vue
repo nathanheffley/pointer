68113f8a939f8faad112d007180eb56c936ee8ca
Nathan Heffley <nathan@nathanheffley.com>
2019-05-31 12:05:23 -0400

    Update Vote Recorder

--- src/components/VoteRecorder.vue
+++ src/components/VoteRecorder.vue
@@ -3,11 +3,11 @@
     <ul class="btn-list sm:max-w-xs">
       <li><button value="0" @click="recordVote(0)" :class="[ isVote(0) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">0</button></li>
       <li><button value="1" @click="recordVote(1)" :class="[ isVote(1) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">1</button></li>
+      <li><button value="2" @click="recordVote(2)" :class="[ isVote(2) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">2</button></li>
       <li><button value="3" @click="recordVote(3)" :class="[ isVote(3) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">3</button></li>
       <li><button value="5" @click="recordVote(5)" :class="[ isVote(5) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">5</button></li>
       <li><button value="8" @click="recordVote(8)" :class="[ isVote(8) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">8</button></li>
       <li><button value="13" @click="recordVote(13)" :class="[ isVote(13) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">13</button></li>
-      <li><button value="20" @click="recordVote(20)" :class="[ isVote(20) ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">20</button></li>
       <li><button value="?" @click="recordVote('?')" :class="[ isVote('?') ? 'btn-blue' : 'btn-grey', 'btn text-xl w-16' ]">?</button></li>
     </ul>
     <div class="flex justify-between mt-2 sm:max-w-xs">
