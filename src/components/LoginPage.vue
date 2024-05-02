9b044fd5180394e5e16756a454c2512cb7f5a1b1
Nathan Heffley <nathan@nathanheffley.com>
2024-05-02 16:14:28 -0400

    Validate session names to contain only letters, numbers, and dashes (#82)
    * Validate session names to contain only letters, numbers, and dashes

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -5,7 +5,7 @@
     </nav>
     <main>
       <form class="px-4 py-6 max-w-md mx-auto">
-        <div class="flex items-center mb-6">
+        <div class="flex items-center">
           <div class="w-1/4">
             <label for="name" class="block font-bold text-right pr-4 text-gray-500">Name</label>
           </div>
@@ -13,7 +13,7 @@
             <input class="login-input" id="name" type="text" required v-model="name" />
           </div>
         </div>
-        <div class="flex items-center mb-6">
+        <div class="flex items-center mt-6">
           <div class="w-1/4">
             <label for="room" class="block text-gray-500 font-bold text-right pr-4">Room</label>
           </div>
@@ -21,10 +21,17 @@
             <input class="login-input" id="room" type="text" required v-model="session" />
           </div>
         </div>
-        <div class="flex items-center">
+        <div v-if="session && session.trim().length > 0 && !isValidSession" class="flex items-center mt-2">
+          <div class="w-1/4">
+          </div>
+          <div class="w-3/4">
+            <p class="text-red-500 text-xs italic">Session name can only contain letters, numbers, and dashes.</p>
+          </div>
+        </div>
+        <div class="flex items-center mt-6">
           <div class="w-1/4"></div>
           <div class="w-3/4">
-            <router-link v-if="(session.trim().length > 0) && (name.trim().length > 0)" :to="{ name: 'SessionPage', params: { id: session.toLowerCase().trim(), name } }" tag="button" class="btn btn-blue no-underline">
+            <router-link v-if="isValidSession && isValidName" :to="{ name: 'SessionPage', params: { id: session.toLowerCase().trim(), name } }" tag="button" class="btn btn-blue no-underline">
               Join
             </router-link>
             <button v-else class="btn btn-blue hover:bg-blue-400 dark:hover:bg-blue-700 opacity-50 cursor-not-allowed" disabled>
@@ -44,6 +51,14 @@ export default {
     return {
       session: '',
       name: ''
+    }
+  },
+  computed: {
+    isValidSession() {
+      return /^[a-zA-Z0-9-]+$/.test(this.session.trim());
+    },
+    isValidName() {
+      return this.name.trim().length > 0;
     }
   },
   mounted () {
