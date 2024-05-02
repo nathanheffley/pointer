fcd862c6e37a268ae2e40e4d766392356407206b
Nathan Heffley <nathan@nathanheffley.com>
2024-05-02 19:22:27 +0000

    Fix width of Login error message

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -21,7 +21,13 @@
             <input class="login-input" id="room" type="text" required v-model="session" />
           </div>
         </div>
-        <p v-if="session && session.trim().length > 0 && !isValidSession" class="mt-2 text-red-500 text-xs italic">Session name can only contain letters, numbers, and dashes.</p>
+        <div class="flex items-center mt-6">
+          <div class="w-1/4">
+          </div>
+          <div class="w-3/4">
+            <p v-if="session && session.trim().length > 0 && !isValidSession" class="mt-2 text-red-500 text-xs italic">Session name can only contain letters, numbers, and dashes.</p>
+          </div>
+        </div>
         <div class="flex items-center mt-6">
           <div class="w-1/4"></div>
           <div class="w-3/4">
